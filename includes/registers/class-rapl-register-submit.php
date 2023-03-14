5ee42723d7a2b2bbbc59f8aa2e5e8d1e9cede0d1
changwoo <ep6tri@hotmail.com>
2023-03-14 16:09:57 +0900

    Nopriv 허용

--- includes/registers/class-rapl-register-submit.php
+++ includes/registers/class-rapl-register-submit.php
@@ -15,7 +15,7 @@ if ( ! class_exists( 'RAPL_Register_Submit' ) ) {
 
 		public function get_items(): Generator {
 			/** @uses RAPL_YouTube_Video::submit_get_video() */
-			yield new RAPL_Reg_Submit( 'rapl_get_video', 'youtube_video@submit_get_video' );
+			yield new RAPL_Reg_Submit( 'rapl_get_video', 'youtube_video@submit_get_video', true );
 		}
 	}
 }
